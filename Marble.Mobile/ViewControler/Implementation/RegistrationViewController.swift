a333424304c51836057faf298070bac42414a0c4
Bianca Minnaar <bminnaar@gmail.com>
2018-03-29 10:24:02 +0200

    Add RegistrationViewController

---
+++ Marble.Mobile/ViewControler/Implementation/RegistrationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegistrationViewController.swift
+//  Marble.Mobile
+//
+//  Created by Bianca Minnaar on 2018/03/29.
+//  Copyright © 2018 MarbleTech. All rights reserved.
+//
+
+import Foundation
---
+++ Marble.Mobile/ViewModel/UserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserViewModel.swift
+//  Marble.Mobile
+//
+//  Created by Bianca Minnaar on 2018/03/29.
+//  Copyright © 2018 MarbleTech. All rights reserved.
+//
+
+import Foundation
+
+class UserViewModel: BaseProjectModel {
+    var userName: String?
+}
